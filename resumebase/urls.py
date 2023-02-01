0c15e0942eefcb25df0778d4684c985fc8678745
cmurray4492 <35826709+cmurray4492@users.noreply.github.com>
2023-01-31 21:28:07 -0600

    second commit

---
+++ resumebase/urls.py
@@ -0,0 +1,9 @@
+"""URLs file for my Resume Base App """
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('', views.index, name='blog'),
+    # path('<int:blog_id>/<str:blog_slug>', views.blogarticle, name='blogarticle'),
+    # path('search', views.search, name='search'),
+]
